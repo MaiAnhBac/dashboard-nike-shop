e6a60411e7ef7655f9655af38f2314959f2f2521
Mai Anh Bắc <maianhbac17052001@gmail.com>
2024-05-23 12:28:26 +0700

    update

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Layout.css';
+export default function Footer() {
+    return (
+        <footer>
+            <div className="footer-item">
+                <div className="footer-item-row">
+                    <span className="item-row-left">2024 © Steex.</span>
+                    <span className="item-row-right">Design & Develop by Themesbrand</span>
+                </div>
+            </div>
+        </footer>
+    )
+}
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import './Layout.css';
 import Navbar from './Navbar'
+import Footer from './Footer';
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
       <main>
         {children}
-        {/* <footer>
-          <div class="footer-item">
-            <div class="footer-item-row">
-              <span class="item-row-left">2024 © Steex.</span>
-              <span class="item-row-right">Design & Develop by Themesbrand</span>
-            </div>
-          </div>
-        </footer> */}
+        <Footer />
       </main>
+      
     </>
   )
 }
